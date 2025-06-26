9eb71e4306a13d4607921069304c0a42f82cc73d
hnbtram <baokhanh20002701@gmail.com>
2025-06-26 14:01:39 +0700

    Create KhachHang.java

---
+++ quanlyquannet/src/KhachHang.java
@@ -0,0 +1,14 @@
+public class KhachHang{
+    int MaKH;
+    string TenKH;
+    string sdt;
+
+    public KhachHang( int MaKH, string TenKH, string sdt){
+        this.MaKH = maKH;
+        this.TenKH = tenKH;
+        this.sdt = sdt;
+    }
+    public void hienthi() {
+        System.out.println("Ten Khach Hang:" + TenKH);
+    }
+}
